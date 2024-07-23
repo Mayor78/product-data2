ac45f23fc191a00ee63ac48f3e72c36ffabd481a
Abraham mayowa <Abrahammayowa78@gmail.com>
2024-07-23 15:11:56 +0100

    test3

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('data/db.json');
+const router = jsonServer.router('data/db.json'); // Adjust the path if necessary
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
